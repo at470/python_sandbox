a1d68e83d7f1f1e3c62e966b2983483d8964bd4d
akiko <kaytodayo@gmail.com>
2022-01-04 09:35:22 +0000

    fuction to convert radians to degrees

---
+++ radians_to_degrees/radians_to_degrees.py
@@ -0,0 +1,10 @@
+# Convert radians into degrees
+
+def radians_to_degrees(radians):
+    # import math library to access value of pi
+    from math import pi
+    # 1° = π/180°
+    degrees = 180 / pi * radians
+    return degrees
+
+# print(radians_to_degrees(3.1))
