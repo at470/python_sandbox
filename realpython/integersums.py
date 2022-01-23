764e37f82edff5cff53c2eeaa2b0aef77ddc82ed
avitus <macair>
2022-01-23 17:55:10 +0000

    integer sums

--- realpython/integersums.py
+++ realpython/integersums.py
@@ -1,10 +1,28 @@
+import unittest
+
+class TestAddItUp(unittest.TestCase):
+
+    def test_when_input_is_1(self):
+        self.assertEqual(add_it_up(1), 1)
+
+    def test_when_input_is_2(self):
+        self.assertEqual(add_it_up(2), 3)
+
+    def test_when_input_is_negative(self):
+        self.assertEqual(add_it_up(-5), 0)
+
+    def test_when_input_is_not_an_int(self):
+        self.assertEqual(add_it_up("Pooch"), 'pal', msg='it\'s not working')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 def add_it_up(input):
+    loop = 0
+    sum = 0
     if type(input) == int:
-        loop = 0
-        sum = 0
-        while loop <= input:
+        while loop < input:
+            loop = loop + 1
             sum = sum + loop
-            loop =+ 1
-        return sum
-    else:
-        return 0
+    return sum
